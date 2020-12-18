ed5d4df30a4a813fd7c865d25bca700fb95047b7
Celeste Staley <celeste@MacBook-Air.local>
2020-12-18 15:54:09 +0000

    added comments

--- tester.R
+++ tester.R
@@ -1,7 +1,12 @@
 tester=function(){
-  
+  # method first create project on github
+  # using the add READme option
+  # now create new project with the the new version control
+  # in the shell set up for ssh git remote set-url origin git@github.com:MRCG-djeffries/testproj3
+  # this assumes you have a key set up in github
+  # check with git remote show origin
   x=0
-  for (i in 1 to 100){
+  for (i in 1 : 100){
        x = x + i
   }
   return(x)
